52ebcfbed4cf12cfb2323f10b5ef0ec65e49ad4b
Victor Dogo <victordogo@gmail.com>
2021-10-05 13:03:48 -0300

    nova limpeza do banco de dados, inicio do relatorio e construcao do readme

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# trab-final-victordogo-r4ds2
+
+Este repositório contém os arquivos, códigos e bancos de dados utilizados para a criação de meu trabalho final do curso "R para Ciência de Dados II".
--- data-raw/csv/Metallica_Data_Clean.R
+++ data-raw/csv/Metallica_Data_Clean.R
@@ -1,15 +1,25 @@
 ## code to prepare `Metallica_Data_Clean` dataset goes here
 
-library(magrittr)
-
-metallica <- readr::read_csv("data-raw/csv/Metallica_Data_Clean.csv") %>%
+metallica <- readr::read_csv("data-raw/csv/Metallica_Data_Clean.csv") |>
+  # Selecting columns of interest
   dplyr::select(Date, Festival, City_Country,
                 Tour, Set, Encores, Set_Length,
-                Has_Guitar_Solo:Hardwired_To_Self_Destruct_Count) %>%
-  dplyr::mutate(Country = purrr::map_chr(
+                Has_Guitar_Solo:Hardwired_To_Self_Destruct_Count) |>
+  # Extracting country from City_Country by splitting string and taking
+  # last element using purrr and stringr
+  dplyr::mutate(
+    Country = purrr::map_chr(
     stringr::str_split(City_Country,pattern = ","), .f=tail,n=1
-  ),.keep="unused") %>%
-  dplyr::mutate(Festival = replace_na("None"))
+  ),
+  .keep="unused", .before=Set) |>
+  # Replacing NA in Festival by "None"
+  # Changing date format in Date column
+  # Replacing NA in Tour by "None"
+  dplyr::mutate(Festival = stringr::str_replace_na(Festival),
+                Festival = stringr::str_replace_all(Festival, "NA", "None"),
+                Tour = stringr::str_replace_na(Tour),
+                Tour = stringr::str_replace_all(Tour, "NA", "None"),
+                Date=lubridate::mdy(data))
 
-metallica %>%
+metallica |>
   readr::write_rds("data/metallica.rds")
